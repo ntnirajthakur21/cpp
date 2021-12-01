37abcdf0d6ba6e07c4b29dc55e58ba2202018efe
ntnirajhthakur21 <ntnirajhthakur21>
2021-12-01 19:04:26 +0545

    adding comments

--- Topic Wise/Array/001.add-20-numbers.cpp
+++ Topic Wise/Array/001.add-20-numbers.cpp
@@ -1,13 +1,20 @@
-#include <iostream>
+//Program in C++ to take input of 20 numbers and display their sum ☺
+// The Program is written by - Niraj Kumar Thakur
+
+#include <iostream>     //Header file
 using namespace std;
-int main()
+int main()              // Main function
 {
     int num[20],sum=0;
+
+    // For Loop will run for 20 times
     for(int i=0;i<20;i++){
         cout<<"Enter number "<<i+1<<"-\> ";
-        cin>>num[i];
-        sum=sum+num[i];
+        cin>>num[i];                // Accepts a number
+        sum=sum+num[i];             // Add the every new number every time
     }
+
+    // Prints the reasult
     cout <<"\n-----------------------------\nThe total sum is -\>"<<sum<<"\n-----------------------------\n";
     return 0;
-}
+}                       // End of main function
